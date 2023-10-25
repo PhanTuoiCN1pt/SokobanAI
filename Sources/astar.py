e5ac3b8b27d4653bdb7627adb771aa0e354fff55
PhanTuoiCN1pt <101046769+PhanTuoiCN1pt@users.noreply.github.com>
2023-10-25 15:02:56 +0700

    Update

--- Sources/astar.py
+++ Sources/astar.py
@@ -64,7 +64,7 @@ def AStar_Search(board, list_check_point):
             
             '''KIỂM TRA XEM TRẠNG THÁI MỚI CÓ PHẢI LÀ TRẠNG THÁI KẾT THÚC KHÔNG'''
             if spf.check_win(new_board, list_check_point):
-                print("\nHeuristic theo khoảng cách Manhattan")
+                print("\nManhattan Distance Heuristic")
                 print("Found Win")
                 print("  Số trạng thái đã duyệt : {} ".format(len(list_state)))
                 process = psutil.Process(os.getpid())
